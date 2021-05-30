aece5ab3b4f320591bce90b173e6af314b459099
PIPE <pipelin56@hotmail.com>
2021-05-30 23:47:24 +0200

    Added qr dependency

--- lib/src/widgets/scan_button.dart
+++ lib/src/widgets/scan_button.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-        elevation: 0, child: Icon(Icons.filter_center_focus), onPressed: () {});
+        elevation: 0,
+        child: Icon(Icons.filter_center_focus),
+        onPressed: () async {
+          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              "#3D8BEF", "Cancel", false, ScanMode.QR);
+          print(barcodeScanRes);
+        });
   }
 }
